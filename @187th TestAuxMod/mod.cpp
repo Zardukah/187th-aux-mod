9aa64792bb616ac5e0ca8ba213bb9a54df21024a
Braydon Nelson <braydon.nelson@outlook.com>
2020-12-02 00:26:43 -0700

    fixed buildpbo.bat

---
+++ @187th TestAuxMod/mod.cpp
@@ -0,0 +1,11 @@
+name = "187th Aux Mod";
+author = "187th Mod Team";
+picture = "187th_logo.paa";
+logo = "187th_logo.paa";
+logoOver = "187th_logo.paa";
+overview = "187th Legion Addons: Glorified Assets";
+description = "187th Mod Collection";
+tooltip = "187th Mod Pack";
+tooltipOwned = "187th Mod Pack Owned";
+overviewPicture = "187th_logo.paa";
+overviewText = "187th Mod Pack";
--- buildpbo.bat
+++ buildpbo.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Building
-"%ProgramFiles%\PBO Manager v.1.4 beta\PBOConsole.exe" -pack ".\Edit Area\187th-aux" ".\@187th Legion Aux Mod\addons\187th-aux.pbo"
+"%ProgramFiles%\PBO Manager v.1.4 beta\PBOConsole.exe" -pack ".\Edit Area\187th-aux" ".\@187th TestAuxMod\addons\187th-aux.pbo"
 echo Finished
-DEL ".\@187th Legion Aux Mod\addons\187th-aux.pbo.bak"
+DEL ".\@187th TestAuxMod\addons\187th-aux.pbo.bak"
 echo Removing Bak
