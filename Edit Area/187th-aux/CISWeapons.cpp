503176fef40b7e8b38e64e0b642846898e65b99b
Braydon Nelson <praetor.panda@gmail.com>
2021-03-22 23:34:04 -0600

    Added base BX Droid
    Added BX Droid
    Added BX Droid Captain
    Added BX Droid Citadel
    Added BX Droid Diplomat
    Added BX Droid Training

--- Edit Area/187th-aux/CISWeapons.cpp
+++ Edit Area/187th-aux/CISWeapons.cpp
@@ -14,7 +14,7 @@ class 187th_b2_Droid: SWOP_B2_superdroid_F_Spec
 		{
 			uniformModel = "-";
 			uniformClass = "187th_CIS_Base_Uniform";
-			containerClass = "Supply40";
+			containerClass = "Supply150";
 			mass = 80;
 		};
     JLTS_isDroid = 1;
@@ -22,3 +22,34 @@ class 187th_b2_Droid: SWOP_B2_superdroid_F_Spec
     JLTS_deathSounds = "DeathDroid";
     class EventHandlers: DefaultEventhandlers{};
 };
+
+//BX Droid E5 - 212th_BX_DROID_F
+class 212th_BX_DROID;
+class 187th_BX_Droid_E5: 212th_BX_DROID
+{
+    author 	= "PraetorPanda";
+    dlc		= "187th Aux Mod";
+    scope 	= 2;
+    scopeCurator = 2;
+    displayName = "BX Droid Chassis";
+    model = "\A3\Characters_F\Common\Suitpacks\suitpack_blufor_diver";
+    class ItemInfo: UniformItem
+		{
+			uniformModel = "-";
+			uniformClass = "187th_BX_Droid_E5_F";
+			containerClass = "Supply150";
+			mass = 80;
+		};
+    JLTS_isDroid = 1;
+    JLTS_hasEMPProtection = 0;
+    JLTS_deathSounds = "DeathDroid";
+    class EventHandlers: DefaultEventhandlers{};
+};
+
+//BX Droid Captain - 212th_BX_DROID_CAPTAIN
+
+//BX Droid Citidel - 212th_BX_DROID_CITADEL
+
+//BX Droid Diplomat - 212th_BX_DROID_DIPLOMAT
+
+//BX Droid Training - 212th_BX_DROID_TRAINING
